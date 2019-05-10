f833b18cc2737340b4ee7695e832b20cfcbe2018
Bas Langenberg <baslangenberg@gmail.com>
2019-05-10 22:25:46 +0000

    Wordpress part one workshop done

---
+++ infrastructure.tf
@@ -0,0 +1,20 @@
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+variable "ssh_fingerprint" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+# Create Kubernetes first master
+resource "digitalocean_droplet" "Wordpress" {
+  image = "ubuntu-18-04-x64"
+  name = "Wordpress"
+  region = "ams3"
+  size = "2gb"
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+}
